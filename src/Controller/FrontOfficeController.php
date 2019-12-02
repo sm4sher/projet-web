f510b2724d062a840493a5834a906709231c706b
Adrien <sm4sher25@gmail.com>
2019-12-02 13:09:23 +0100

    ajout/modification/suppresion du panier

--- src/Controller/FrontOfficeController.php
+++ src/Controller/FrontOfficeController.php
@@ -3,20 +3,24 @@
 namespace App\Controller;
 
 use App\Repository\EvenementRepository;
+use App\Repository\PanierPlaceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class FrontOfficeController extends AbstractController
 {
     /**
      * @Route("/front/office", name="front_office")
      */
-    public function index(Request $request, EvenementRepository $evenementRepository)
+    public function index(Request $request, Security $security, EvenementRepository $evenementRepository, PanierPlaceRepository $panierPlaceRepository)
     {
         $events = $evenementRepository->findAll();
+        $panier = $panierPlaceRepository->findBy(['user' => $security->getUser()]);
         return $this->render('frontOff/frontOFFICE.html.twig', [
             'events' => $events,
+            'panier' => $panier
         ]);
     }
 }
--- src/Controller/PanierPlaceController.php
+++ src/Controller/PanierPlaceController.php
@@ -12,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * @Route("/panier/place")
@@ -28,9 +29,10 @@ class PanierPlaceController extends AbstractController
         ]);
     }
 
-    /**
+/*    /**
      * @Route("/new", name="panier_place_new", methods={"GET","POST"})
      */
+/*
     public function new(Request $request, Evenement $evenement = null): Response
     {
         $panierPlace = new PanierPlace();
@@ -57,33 +59,48 @@ class PanierPlaceController extends AbstractController
             'panier_place' => $panierPlace,
             'form' => $form->createView(),
         ]);
-    }
+    }*/
 
     /**
      * @Route("/{id}/add", name="panier_place_add", methods={"GET", "POST"})
      */
-    public function add(Request $request, Evenement $evenement, ObjectManager $manager)
+    public function add(Request $request, Evenement $evenement, ObjectManager $manager, Security $security, $id)
     {
-        $panierPlace = new PanierPlace();
-        $form = $this->createForm(PanierPlaceType::class, $panierPlace);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            if (!$panierPlace->getId()) {
-                $panierPlace->setDateAchat(new \DateTime)
-                    ->setUser($this->getUser())
-                    ->setEvenement($this->getEvenement());
-            }
-            $manager->persist($panierPlace);
-            $manager->flush();
-
-            return $this->redirectToRoute('panier_place_index');
+        $evenement = $this->getDoctrine()->getRepository(Evenement::class)->findOneBy(['id' => $id]);
+        $panierPlace = $this->getDoctrine()->getRepository(PanierPlace::class)->findOneBy(['user' => $security->getUser(), 'evenement' => $evenement]);
+        if(!$panierPlace){
+            $panierPlace = new PanierPlace();
+            $panierPlace->setEvenement($evenement);
+            $panierPlace->setUser($security->getUser());
+            $panierPlace->setQuantite(1);
+            $panierPlace->setDateAchat(new \DateTime());
+        } else {
+            $panierPlace->setQuantite($panierPlace->getQuantite() + 1);
         }
-        return $this->render('panier_place/new.html.twig', [
-            'panier_place' => $panierPlace,
-            'evenement' => $evenement,
-            'form' => $form->createView(),
-        ]);
+        $manager->persist($panierPlace);
+        $manager->flush();
+
+        return $this->redirectToRoute('front_office');
+
+//        $form = $this->createForm(PanierPlaceType::class, $panierPlace);
+//        $form->handleRequest($request);
+
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            if (!$panierPlace->getId()) {
+//                $panierPlace->setDateAchat(new \DateTime)
+//                    ->setUser($this->getUser())
+//                    ->setEvenement($this->getEvenement());
+//            }
+//            $manager->persist($panierPlace);
+//            $manager->flush();
+//
+//            return $this->redirectToRoute('panier_place_index');
+//        }
+//        return $this->render('panier_place/new.html.twig', [
+//            'panier_place' => $panierPlace,
+//            'evenement' => $evenement,
+//            'form' => $form->createView(),
+//        ]);
     }
 
     /**
@@ -97,11 +114,20 @@ class PanierPlaceController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/edit", name="panier_place_edit", methods={"GET","POST"})
+     * @Route("/{id}/edit", name="panier_place_edit", methods={"POST"})
      */
-    public function edit(Request $request, PanierPlace $panierPlace): Response
+    public function edit(Request $request, Security $security, ObjectManager $manager, $id): Response
     {
-        $form = $this->createForm(PanierPlaceType::class, $panierPlace);
+        $evenement = $this->getDoctrine()->getRepository(Evenement::class)->findOneBy(['id' => $id]);
+        $panierPlace = $this->getDoctrine()->getRepository(PanierPlace::class)->findOneBy(['user' => $security->getUser(), 'evenement' => $evenement]);
+        if($panierPlace){
+            $panierPlace->setQuantite($request->get("quantite"));
+            $manager->persist($panierPlace);
+            $manager->flush();
+        }
+        return $this->redirectToRoute("front_office");
+
+        /*$form = $this->createForm(PanierPlaceType::class, $panierPlace);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -113,20 +139,28 @@ class PanierPlaceController extends AbstractController
         return $this->render('panier_place/edit.html.twig', [
             'panier_place' => $panierPlace,
             'form' => $form->createView(),
-        ]);
+        ]);*/
     }
 
     /**
-     * @Route("/{id}", name="panier_place_delete", methods={"DELETE"})
+     * @Route("/{id}/delete", name="panier_place_delete", methods={"GET"})
      */
-    public function delete(Request $request, PanierPlace $panierPlace): Response
+    public function delete(Request $request, Security $security, ObjectManager $manager, $id): Response
     {
-        if ($this->isCsrfTokenValid('delete' . $panierPlace->getId(), $request->request->get('_token'))) {
+        $evenement = $this->getDoctrine()->getRepository(Evenement::class)->findOneBy(['id' => $id]);
+        $panierPlace = $this->getDoctrine()->getRepository(PanierPlace::class)->findOneBy(['user' => $security->getUser(), 'evenement' => $evenement]);
+        if($panierPlace){
+            $manager->remove($panierPlace);
+            $manager->flush();
+        }
+        return $this->redirectToRoute("front_office");
+
+        /*if ($this->isCsrfTokenValid('delete' . $panierPlace->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($panierPlace);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('panier_place_index');
+        return $this->redirectToRoute('panier_place_index');*/
     }
 }
