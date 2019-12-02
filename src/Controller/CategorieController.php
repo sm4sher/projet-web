b8f3b994262affe55fe8d01afed04e4b8895f5e0
Adrien <sm4sher25@gmail.com>
2019-12-02 20:18:23 +0100

    petit oubli

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Categorie;
 use App\Form\CategorieType;
 use App\Repository\CategorieRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
