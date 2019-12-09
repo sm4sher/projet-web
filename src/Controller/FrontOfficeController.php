befd5ca042e7d698cf9176ece11b2ad86c631d47
Adrien <sm4sher25@gmail.com>
2019-12-09 14:06:29 +0100

    Filtre

--- src/Controller/FrontOfficeController.php
+++ src/Controller/FrontOfficeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategorieRepository;
 use App\Repository\EvenementRepository;
 use App\Repository\PanierPlaceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,12 +15,31 @@ class FrontOfficeController extends AbstractController
     /**
      * @Route("/evenements", name="front_office")
      */
-    public function index(Request $request, Security $security, EvenementRepository $evenementRepository, PanierPlaceRepository $panierPlaceRepository)
+    public function index(Request $request, Security $security, EvenementRepository $evenementRepository)
     {
         $events = $evenementRepository->findAll();
-        $panier = $panierPlaceRepository->findBy(['user' => $security->getUser()]);
         return $this->render('frontOff/frontOFFICE.html.twig', [
             'events' => $events
+        ]);
+    }
+
+    /**
+     * @Route("/evenements/filtre", name="front_office_filtre", methods={"GET", "POST"})
+     */
+    public function filtre(Request $request, Security $security, EvenementRepository $evenementRepository, CategorieRepository $categorieRepository)
+    {
+        $categories = $categorieRepository->findAll();
+        if($request->request->get('categorie')) {
+            $categorie = $categorieRepository->find($request->request->get("categorie"));
+            $events = $evenementRepository->findBy(['categorie' => $categorie]);
+        }
+        else {
+            $events = $evenementRepository->findAll();
+        }
+        return $this->render('frontOff/filtre.html.twig', [
+            'categories' => $categories,
+            'selected' => $request->request->get('categorie'),
+            'evenements' => $events
         ]);
     }
 }
